f466c6cc51839d0551318a608cdbc90dd6632489
Виктория Смирнова <vismirnova-0407@mail.ru>
2024-10-23 17:46:08 +0700

    Цикл в шаблоне

--- app.py
+++ app.py
@@ -472,4 +472,12 @@ def example():
     lab_number = '2'
     group = 'ФБИ-21'
     course = '3 курс'
-    return render_template('example.html', name=name, lab_number=lab_number, group=group, course=course)
+    fruits = [
+        {'name': 'яблоки', 'price': 100},
+        {'name': 'груши', 'price': 120},
+        {'name': 'апельсины', 'price': 80},
+        {'name': 'мандарины', 'price': 95},
+        {'name': 'манго', 'price': 321},
+    ]
+    return render_template('example.html', name=name, lab_number=lab_number, 
+                           group=group, course=course, fruits=fruits)
