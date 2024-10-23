fb07d70e8d58e12d886bfee6a321716fbb2624db
Виктория Смирнова <vismirnova-0407@mail.ru>
2024-10-23 16:46:49 +0700

    Имя студента

--- app.py
+++ app.py
@@ -468,4 +468,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Виктория Смирнова'
+    return render_template('example.html', name=name)
