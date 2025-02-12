0ace124789eda57865e4f9a71bce175d3eabfaba
Виктория Смирнова <vismirnova-0407@mail.ru>
2025-02-13 03:16:32 +0700

    Настройки отображения

--- lab3.py
+++ lab3.py
@@ -75,3 +75,33 @@ def success():
     else:
         price = request.args.get('price') 
     return render_template('/lab3/success.html', price=price)
+
+
+@lab3.route('/lab3/settings')
+def settings():
+    color = request.args.get('color')
+    background_color = request.args.get('background_color')
+    font_size = request.args.get('font_size')
+    padding = request.args.get('padding')
+    
+    if color or background_color or font_size:
+        resp = make_response(redirect('/lab3/settings'))
+        if color:
+            resp.set_cookie('color', color)
+        if background_color:
+            resp.set_cookie('background_color', background_color)
+        if font_size:
+            resp.set_cookie('font_size', font_size)
+        if padding:
+            resp.set_cookie('padding', padding)
+        return resp
+    
+    color = request.cookies.get('color')
+    background_color = request.cookies.get('background_color')
+    font_size = request.cookies.get('font_size')
+    
+    resp = make_response(render_template('lab3/settings.html', color=color, 
+                                         background_color=background_color,
+                                          font_size=font_size,padding=padding))
+    return resp
+
