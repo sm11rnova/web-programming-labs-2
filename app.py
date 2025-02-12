529ba2cf78ef21b5f7cc80414122cae0534af658
Виктория Смирнова <vismirnova-0407@mail.ru>
2025-02-13 05:22:36 +0700

    Деление

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, url_for, redirect, render_template, abort, request
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
+from lab4 import lab4
 
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
+app.register_blueprint(lab4)
 
 
 
@@ -30,6 +32,7 @@ def start():
             <li><a href="/lab1>Первая лабораторная</a></li>
             <li><a href="/lab2>Вторая лабораторная</a></li>
             <li><a href="/lab3/">Третья лабораторная</a></li>
+            <li><a href="/lab4/">Четвёртная лабораторная</a></li>
         </ul>
         <footer>
             <p>ФИО: Смирнова Виктория Александровна</p>
